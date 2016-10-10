56ab1aa91850dc41f36c5481c4045f72b5bfc422
xdlv <kkjattuu@163.com>
2016-10-10 22:33:54 +0800

    add env scripts

---
+++ bin/setenv.bat
@@ -0,0 +1,2 @@
+set CATALINA_OPTS=-Djava.awt.headless=true -Djava.net.preferIpv4Stack=true
+rem set JAVA_OPTS=-server -Xmx100m
---
+++ bin/setenv.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+CATALINA_OPTS="-Djava.awt.headless=true -Djava.net.preferIpv4Stack=true"
+# JAVA_OPTS="-server -Xmx100m"
